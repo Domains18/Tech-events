caa2d9c4eec4537b1debed904db155a86ffba7bb
Domains18 <gibsonsgibson88@gmail.com>
2023-04-17 17:43:34 +0300

    events
    Signed-off-by: Domains18 <gibsonsgibson88@gmail.com>

--- front/src/Router.js
+++ front/src/Router.js
@@ -4,7 +4,7 @@ import { Navigate } from "react-router-dom";
 import App from "./App";
 import DefaultLayout from "./Layouts/DefaultLayout";
 import UserLayout from "./Layouts/UserLayout";
-import UpcomingEvents from './Events/UpcomingEvents/UpcomingEvents';
+import UpcomingEvents from './Components/Events/UpcomingEvents/UpcomingEvents';
 import About from './Components/About/AboutUs';
 import Contact from './Components/Contact/Contact';
 import Search from './Views/Search/Search';
---
+++ front/src/Views/FindEvents/Spawn.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const spawn = () => {
+  return (
+    <div>spawn</div>
+  )
+}
+
+export default spawn
