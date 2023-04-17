9590a45469cb0d4b9f9edac2645a4ab4dac32434
Domains18 <gibsonsgibson88@gmail.com>
2023-04-17 19:35:16 +0300

    events
    Signed-off-by: Domains18 <gibsonsgibson88@gmail.com>

--- front/src/Views/FindEvents/FindEvents.js
+++ front/src/Views/FindEvents/FindEvents.js
@@ -3,8 +3,9 @@ import './FindEvents.scss'
 const FindEvents = () => {
   return (
     <div className='findevents'>
-      <div>
+      <div className='findevents'>
         <h1>Find Events Near You</h1>
+        <input type="text"  placeholder='Find Event By City'/>
       </div>
     </div>
   )
