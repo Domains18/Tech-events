85bc81a1c2fe921e94a272dcc9c89a77fe220636
Domains18 <gibsonsgibson88@gmail.com>
2023-04-17 20:16:11 +0300

    events
    Signed-off-by: Domains18 <gibsonsgibson88@gmail.com>

--- front/src/Views/FindEvents/FindEvents.js
+++ front/src/Views/FindEvents/FindEvents.js
@@ -2,10 +2,12 @@ import React from 'react'
 import './FindEvents.scss'
 const FindEvents = () => {
   return (
-    <div className='findevents'>
-      <div>
-        <h1>Find Events Near You</h1>
-        <input type="text"  placeholder='Find Event By City'/>
+    <div className='wrapper'>
+      <div className='findevents'>
+        <div>
+          <h1>Find Events Near You</h1>
+          <input type="text" placeholder='Find Event By City' />
+        </div>
       </div>
     </div>
   )
